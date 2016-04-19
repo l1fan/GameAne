eefa2d8c61dbfa5f53344586cc654e05a1cd2b25
l1fan <yiyimx@gmail.com>
2016-04-19 13:32:56 +0800

    added interface of google's getAdID() and isAdEnable() 初始化返回是否成功事件，如果成功则可以正常调用上面接口获取广告ID 如果初始化失败调用相关接口会报错。 isAdEnable 用户是否允许追踪广告ID getAdID 获取用户的广告ID

--- src/java/com/l1fan/ane/appsflyer/SDK.java
+++ src/java/com/l1fan/ane/appsflyer/SDK.java
@@ -1,20 +1,49 @@
 package com.l1fan.ane.appsflyer;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.Activity;
+
 import com.appsflyer.AppsFlyerLib;
+import com.google.android.gms.ads.identifier.AdvertisingIdClient;
+import com.google.android.gms.ads.identifier.AdvertisingIdClient.Info;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.l1fan.ane.SDKContext;
 
 public class SDK extends SDKContext {
 	
+	private Info mAdInfo;
+
 	public void init() throws JSONException {
 		JSONObject init = getJsonData();
 		AppsFlyerLib.setAppsFlyerKey(init.optString("devKey","kFZnPcRtTVMmje5YeX5dAX"));
 		AppsFlyerLib.sendTracking(getActivity());
+		final Activity activity = getActivity();
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					System.out.println("current thread:"+Thread.currentThread().getName());
+					mAdInfo = AdvertisingIdClient.getAdvertisingIdInfo(activity);
+					System.out.println("---dispatch---");
+					dispatchData(EVENT_INIT, "google play adid init success");
+				} catch (IllegalStateException
+						| GooglePlayServicesRepairableException | IOException
+						| GooglePlayServicesNotAvailableException e) {
+					dispatchError(EVENT_INIT, e.getMessage());
+					e.printStackTrace();
+				}	
+			}
+		}).start();
+		
+
 	}
 	
 	public void track() throws JSONException{
@@ -33,7 +62,24 @@ public class SDK extends SDKContext {
 		AppsFlyerLib.trackEvent(getActivity(), type, map);
 	}
 	
-	public  String getAppsFlyerUID(){
+	public String getAppsFlyerUID(){
 		return AppsFlyerLib.getAppsFlyerUID(getActivity());
+	}
+	
+	public String getAdID(){
+		if (mAdInfo == null) {
+			dispatchError(EVENT_ERROR, "google play adid info is null , please check init info");
+			return "";
+		}
+		return mAdInfo.getId();
+
+	}
+	
+	public boolean isAdEnable(){
+		if (mAdInfo == null) {
+			dispatchError(EVENT_ERROR, "google play adid info is null , please check init info");
+			return false;
+		}
+		return !mAdInfo.isLimitAdTrackingEnabled();
 	}
 }
